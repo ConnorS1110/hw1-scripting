0106efab24f08482c16440bd233c2bc05a1cc801
ldu2 <ldu2@ncsu.edu>
2023-01-18 14:34:29 -0500

    file sperates

--- src/working [DO NOT MODIFY]/main.py
+++ src/working [DO NOT MODIFY]/main.py
@@ -1,63 +1,8 @@
 import argparse
 import sys
 import math
-
-class NUM:
-    def __init__(self):
-        self.n = 0
-        self.mu = 0
-        self.m2 = 0
-        self.lo = float('inf')
-        self.hi = float('-inf') # Replaced sys.maxsize
-
-    def add(self, n):
-        # print("Inside Add", n)
-        if n != "?": # Why Question mark
-            self.n += 1
-            d = n - self.mu
-            self.mu += d / self.n
-            self.m2 += d * (n - self.mu)
-            self.lo = min(n, self.lo)
-            self.hi = max(n, self.hi)
-        # print(self.n, d, self.mu, self.m2, self.lo, self.hi)
-
-    def mid(self):
-        return self.mu
-
-    def div(self): # Removed x
-        return (self.m2 < 0 or self.n < 2) and 0 or (self.m2 / (self.n - 1)) ** 0.5
-
-
-
-
-
-class SYM:
-    def __init__(self):
-        self.n = 0
-        self.has = {}
-        self.most = 0
-        self.mode = None
-
-    def add(self, x):
-        if x != "?":
-            self.n += 1
-            self.has[x] = 1 + self.has.get(x, 0)  # Return to later for dictionary
-            if self.has[x] > self.most:
-                self.most = self.has[x]
-                self.mode = x
-
-    def mid(self):
-        return self.mode
-
-    def div(self): # Removed all parameters
-        def fun(p):
-            return p * math.log(p, 2)
-        
-        e = 0
-        for _, value in self.has.items():
-            e += fun(value/self.n)
-        return -e
-
+import num
+import sym
 
 ###################################################
 
